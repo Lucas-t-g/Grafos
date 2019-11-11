eb17e19615b1d31f432a230eba8acff0d6c24205
Lucas-t-g <lucastarsitanogarcia@gmail.com>
2019-11-11 12:53:47 -0300

    implementada a função que faz busca em profundidade por matriz de adjacencia

--- grafos_matriz.py
+++ grafos_matriz.py
@@ -10,22 +10,32 @@ e) Visualizar o grafo -feito
 f) Identificar Fontes e sumidouros -feito
 g) Calcular o grau de entrada e o grau de saída de um vértice -feito
 h) Busca em largura
-i) busca em profundidade
+i) busca em profundidade -feita
 j) Implmentar os algoritmos Prim e Kruskal
 """
 
 class vertice:
    def __init__(self, conteudo):
-      self.conteudo = conteudo
+      self.conteudo = conteudo      #referece ao conteudo do vertice
+      self.adjacente = []           #os adjacentes ao vertice é uma lista de vertices
+      self.bandeira_de_visita = 0
+      self.distancia = None
+      self.pai = None
 
 class grafo_matriz:
    def __init__(self, lista = []):
       tam = len(lista)
       self.vertices = lista
+      
+      self.vertices_2 = []
+      for elem in lista:
+         self.vertices_2.append(vertice(elem))
+
       self.matriz_adj = np.zeros([tam, tam])
 
    def adicona_vertice(self, conteudo):
       self.vertices.append(conteudo)
+      self.vertices_2.append(vertice(conteudo))
       tam = len(self.vertices)
       nova_coluna = np.zeros([tam-1, 1])
       self.matriz_adj = np.concatenate((self.matriz_adj, nova_coluna), axis=1)
@@ -75,8 +85,19 @@ class grafo_matriz:
             _str += " é sumidouro"
          print(_str)
 
-   #def busca_em_profundidade(self):
-      
+   def busca_em_profundidade(self, vertice_u):
+      for i in range(len(self.vertices)):
+         if ( self.vertices[i] == vertice_u ):
+            print( self.vertices[i] )
+            self.vertices_2[i].bandeira_de_visita = 1
+            break
+
+      for j in range(len(self.vertices)):
+         if(self.matriz_adj[i, j] != 0 and self.vertices_2[j].bandeira_de_visita == 0):
+            self.busca_em_profundidade(self.vertices[j])
+            print("backtrack")
+
+
 
 
 #####__INICIO__DA_EXECUÇÃO__#####
@@ -92,5 +113,6 @@ grafo.adiciona_aresta("y", "w")
 #grafo.mostra_grafo()
 grafo.remove_areseta("z", "a")
 grafo.mostra_grafo()
-grafo.grau_de_entrada_e_saida( "z",print_flag = True)
-grafo.indentifica_fontes_e_sumidouros()
+#grafo.grau_de_entrada_e_saida( "z",print_flag = True)
+#grafo.indentifica_fontes_e_sumidouros()
+grafo.busca_em_profundidade("w")
